38bc4d8c46041e605a1b85cd6b54289c9d40480b
Jiří Slischka <slischkaj@gmail.com>
2023-04-28 16:00:11 +0200

    Result

--- src/Main.php
+++ src/Main.php
@@ -1,10 +1,98 @@
 <?php
+
 namespace App;
 
 class Main
 {
-    public function index(): int
-    {
-        return 0;
-    }
+	const map = [
+		1 => 'I',
+		4 => 'IV',
+		5 => 'V',
+		9 => 'IX',
+		10 => 'X',
+		40 => 'XL',
+		50 => 'L',
+		90 => 'XC',
+		100 => 'C',
+		400 => 'CD',
+		500 => 'D',
+		900 => 'CM',
+		1000 => 'M',
+	];
+
+	public function convert(int $number): string|false
+	{
+		return match ($number) {
+			1 => 'I',
+			4 => 'IV',
+			5 => 'V',
+			9 => 'IX',
+			10 => 'X',
+			40 => 'XL',
+			50 => 'L',
+			90 => 'XC',
+			100 => 'C',
+			400 => 'CD',
+			500 => 'D',
+			900 => 'CM',
+			1000 => 'M',
+			default => false,
+		};
+	}
+
+	public function getArray(int $number): array
+	{
+		$array = [];
+		// get array of numbers from arabic number
+		do {
+			$low = $this->findClosesLow($number);
+			$array[] = $low;
+			$number -= $low;
+		} while ($number > 0);
+
+//		$nums = str_split(strrev((string)$number));
+//		$array = [];
+//
+//		foreach ($nums as $key) {
+//			$array[] = $key;
+//		}
+
+		return $array;
+	}
+
+	public function parse(int $number): string
+	{
+		$array = $this->getArray($number);
+		$str = '';
+		foreach ($array as $a) {
+			$str .= $this->convert($a);
+		}
+		return $str;
+	}
+
+	public function findClosesLow(int $number): int|false
+	{
+		$var = false;
+
+		foreach (array_reverse([1000,900, 500,400, 100,90, 50,40, 10, 5,4, 1]) as $value) {
+			if ($value <= $number) {
+				$var = $value;
+			}
+		}
+
+		return $var;
+	}
+
+	public function findClosestHigh(int $number): int|false
+	{
+		$var = false;
+
+		foreach ([1000, 500, 100, 50, 10, 5, 1] as $value) {
+			if ($value > $number) {
+				$var = $value;
+			}
+		}
+
+		return $var;
+	}
 }
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -5,12 +5,45 @@ use PHPUnit\Framework\TestCase;
 class MainTest extends TestCase
 {
     /**
-     * @covers \App\Main::index
+     * @covers \App\Main::convert
      */
     public function testIndex(): void
     {
         $main = new Main();
-        $this->assertEquals(0, $main->index());
+        $this->assertEquals("I", $main->convert(1));
+        $this->assertEquals("V", $main->convert(5));
+        $this->assertEquals("X", $main->convert(10));
+        $this->assertEquals("L", $main->convert(50));
+        $this->assertEquals("C", $main->convert(100));
+        $this->assertEquals("D", $main->convert(500));
+        $this->assertEquals("M", $main->convert(1000));
+        $this->assertEquals(false, $main->convert(6));
+
+		$this->assertEquals('V', $main->parse(5));
+//		$this->assertEquals('IV', $main->parse(4));
+//		$this->assertEquals('VI', $main->parse(6));
+//		$this->assertEquals('V', $main->parse(5));
+		$this->assertEquals(10, $main->findClosesLow(14));
+		$this->assertEquals(10, $main->findClosesLow(10));
+		$this->assertEquals([], $main->getArray(2587));
+//		$this->assertEquals('V', $main->parse(5));
+//		$this->assertEquals('V', $main->parse(5));
+
+
+//
+//		$this->assertEquals(5, $main->findClosestHigh(4));
+//		$this->assertEquals(10, $main->findClosestHigh(9));
+//		$this->assertEquals(100, $main->findClosestHigh(60));
+//		$this->assertEquals(100, $main->findClosestHigh(70));
+//
+//		$this->assertEquals("I", $main->parse(144));
+
+
+
+
+//        $this->assertEquals([1,5], $main->getArray(4));
+//        $this->assertEquals([1,1,1], $main->getArray(3));
+
     }
 
 }
